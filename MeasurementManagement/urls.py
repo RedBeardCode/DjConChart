35872feef897719dfe946d618a4efa4e08956920
farmer <farmi@web.de>
2015-09-02 12:17:36 +0200

    Datepicker mit nur einen Today und Login_required eingebaut

--- MeasurementManagement/urls.py
+++ MeasurementManagement/urls.py
@@ -2,11 +2,14 @@ __author__ = 'farmer'
 
 from django.conf.urls import patterns, url#
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
+
 from .views import MeasurementView
+
 admin.autodiscover()
 
 
 urlpatterns = patterns('MeasurementManagement.views',
-                       url(r'^new_measurement/$', MeasurementView.as_view(), name="new_measurement"),
+                       url(r'^new_measurement/$', login_required(MeasurementView.as_view()), name="new_measurement"),
 
                       )
