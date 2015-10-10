137f93c168c3078002084e0be6754f671c722227
farmer <farmi@web.de>
2015-10-10 18:11:17 +0300

    Selenium Admin tests eingebaut

--- MeasurementManagement/tests/test_admin_site.py
+++ MeasurementManagement/tests/test_admin_site.py
@@ -1,1 +1,126 @@
-__author__ = 'farmer'
+import pytest
+from selenium import webdriver
+
+from .utilies import login_as_admin, create_correct_sample_data
+
+
+@pytest.mark.django_db
+def test_admin_start(admin_client, live_server):
+    create_correct_sample_data()
+    selenium = webdriver.Firefox()
+    selenium.get(live_server + '/admin/')
+    login_as_admin(selenium)
+    assert selenium.find_elements_by_link_text('Calculation rules')
+    assert selenium.find_elements_by_link_text('Characteristic value descriptions')
+    assert selenium.find_elements_by_link_text('Characteristic values')
+    assert selenium.find_elements_by_link_text('Measurement devices')
+    assert selenium.find_elements_by_link_text('Measurement items')
+    assert selenium.find_elements_by_link_text('Measurement order definitions')
+    assert selenium.find_elements_by_link_text('Measurement orders')
+    assert selenium.find_elements_by_link_text('Measurements')
+    selenium.close()
+
+
+@pytest.mark.django_db
+def test_admin_calculation_rule(admin_client, live_server):
+    create_correct_sample_data()
+    selenium = webdriver.Firefox()
+    selenium.get(live_server + '/admin/MeasurementManagement/calculationrule/')
+    login_as_admin(selenium)
+    assert selenium.find_elements_by_link_text('dummy')
+    tbody = selenium.find_element_by_id('result_list').find_element_by_tag_name('tbody')
+    assert tbody
+    lines = tbody.find_elements_by_tag_name('tr')
+    assert len(tbody.find_elements_by_tag_name('tr')) == 1
+    selenium.close()
+
+
+@pytest.mark.django_db
+def test_admin_characteristic_value_descriptions(admin_client, live_server):
+    create_correct_sample_data()
+    selenium = webdriver.Firefox()
+    selenium.get(live_server + '/admin/MeasurementManagement/characteristicvaluedescription/')
+    login_as_admin(selenium)
+    tbody = selenium.find_element_by_id('result_list').find_element_by_tag_name('tbody')
+    assert tbody
+    assert len(tbody.find_elements_by_tag_name('tr')) == 3
+    assert tbody.find_element_by_link_text('height')
+    assert tbody.find_element_by_link_text('width')
+    assert tbody.find_element_by_link_text('length')
+    selenium.close()
+
+
+@pytest.mark.django_db
+def test_admin_measurement_device(admin_client, live_server):
+    create_correct_sample_data()
+    selenium = webdriver.Firefox()
+    selenium.get(live_server + '/admin/MeasurementManagement/measurementdevice/')
+    login_as_admin(selenium)
+    tbody = selenium.find_element_by_id('result_list').find_element_by_tag_name('tbody')
+    assert tbody
+    assert len(tbody.find_elements_by_tag_name('tr')) == 5
+    assert tbody.find_element_by_link_text('Device 4: 4')
+    assert tbody.find_element_by_link_text('Device 3: 3')
+    assert tbody.find_element_by_link_text('Device 2: 2')
+    assert tbody.find_element_by_link_text('Device 1: 1')
+    assert tbody.find_element_by_link_text('Device 0: 0')
+    selenium.close()
+
+
+@pytest.mark.django_db
+def test_admin_measurement_item(admin_client, live_server):
+    create_correct_sample_data()
+    selenium = webdriver.Firefox()
+    selenium.get(live_server + '/admin/MeasurementManagement/measurementitem/')
+    login_as_admin(selenium)
+    tbody = selenium.find_element_by_id('result_list').find_element_by_tag_name('tbody')
+    assert tbody
+    assert len(tbody.find_elements_by_tag_name('tr')) == 10
+    assert tbody.find_element_by_link_text('Item 9: 9')
+    assert tbody.find_element_by_link_text('Item 8: 8')
+    assert tbody.find_element_by_link_text('Item 7: 7')
+    assert tbody.find_element_by_link_text('Item 6: 6')
+    assert tbody.find_element_by_link_text('Item 5: 5')
+    assert tbody.find_element_by_link_text('Item 4: 4')
+    assert tbody.find_element_by_link_text('Item 3: 3')
+    assert tbody.find_element_by_link_text('Item 2: 2')
+    assert tbody.find_element_by_link_text('Item 1: 1')
+    assert tbody.find_element_by_link_text('Item 0: 0')
+    selenium.close()
+
+
+@pytest.mark.django_db
+def test_admin_measurement_order_definition(admin_client, live_server):
+    create_correct_sample_data()
+    selenium = webdriver.Firefox()
+    selenium.get(live_server + '/admin/MeasurementManagement/measurementorderdefinition/')
+    login_as_admin(selenium)
+    tbody = selenium.find_element_by_id('result_list').find_element_by_tag_name('tbody')
+    assert tbody
+    assert len(tbody.find_elements_by_tag_name('tr')) == 3
+    assert tbody.find_element_by_link_text('OrderDefinition3')
+    assert tbody.find_element_by_link_text('OrderDefinition2')
+    assert tbody.find_element_by_link_text('OrderDefinition1')
+    selenium.close()
+
+
+@pytest.mark.django_db
+def test_admin_measurement_order(admin_client, live_server):
+    create_correct_sample_data()
+    selenium = webdriver.Firefox()
+    selenium.get(live_server + '/admin/MeasurementManagement/measurementorder/')
+    login_as_admin(selenium)
+    tbody = selenium.find_element_by_id('result_list').find_element_by_tag_name('tbody')
+    assert tbody
+    assert len(tbody.find_elements_by_tag_name('tr')) == 10
+    assert tbody.find_element_by_link_text('OrderDefinition1 from 2020-12-14 17:05:55+00:00')
+    assert tbody.find_element_by_link_text('OrderDefinition3 from 2020-12-13 17:05:55+00:00')
+    assert tbody.find_element_by_link_text('OrderDefinition2 from 2020-12-12 17:05:55+00:00')
+    assert tbody.find_element_by_link_text('OrderDefinition1 from 2020-12-11 17:05:55+00:00')
+    assert tbody.find_element_by_link_text('OrderDefinition3 from 2020-12-10 17:05:55+00:00')
+    assert tbody.find_element_by_link_text('OrderDefinition2 from 2020-12-09 17:05:55+00:00')
+    assert tbody.find_element_by_link_text('OrderDefinition1 from 2020-12-08 17:05:55+00:00')
+    assert tbody.find_element_by_link_text('OrderDefinition3 from 2020-12-07 17:05:55+00:00')
+    assert tbody.find_element_by_link_text('OrderDefinition2 from 2020-12-06 17:05:55+00:00')
+    assert tbody.find_element_by_link_text('OrderDefinition1 from 2020-12-05 17:05:55+00:00')
+    selenium.close()
--- MeasurementManagement/tests/test_new_measurement.py
+++ MeasurementManagement/tests/test_new_measurement.py
@@ -4,50 +4,11 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
-from MeasurementManagement.models import MeasurementOrder, MeasurementOrderDefinition, CharacteristicValueDescription
-from MeasurementManagement.models import MeasurementDevice, MeasurementItem, CalculationRule
-
+from .utilies import login_as_admin, create_correct_sample_data
 
 
 # Create your tests here.
 
-
-FAKE_TIME = datetime.datetime(2020, 12, 5, 17, 5, 55)
-
-
-def login_as_admin(selenium):
-    username = selenium.find_element_by_id('id_username')
-    pwd = selenium.find_element_by_id('id_password')
-    username.send_keys('admin')
-    pwd.send_keys('password')
-    selenium.find_element_by_tag_name('form').submit()
-
-
-def create_correct_sample_data():
-    calc_rule = CalculationRule.objects.create(rule_name="dummy", rule_code='def dummy():\n    return True\n')
-    devices = [MeasurementDevice.objects.create(sn=i, name='Device {:d}'.format(i)) for i in range(5)]
-    length = CharacteristicValueDescription.objects.create(value_name='length', description='length',
-                                                           calculation_rule=calc_rule)
-    length.possible_meas_devices.add(devices[0])
-    width = CharacteristicValueDescription.objects.create(value_name='width', description='width',
-                                                          calculation_rule=calc_rule)
-    width.possible_meas_devices.add(*(devices[:3]))
-    height = CharacteristicValueDescription.objects.create(value_name='height', description='height',
-                                                           calculation_rule=calc_rule)
-    height.possible_meas_devices.add(*devices)
-
-    order_definition1 = MeasurementOrderDefinition.objects.create(name="OrderDefinition1")
-    order_definition1.charateristic_values.add(length)
-    order_definition2 = MeasurementOrderDefinition.objects.create(name="OrderDefinition2")
-    order_definition2.charateristic_values.add(length, width)
-    order_definition3 = MeasurementOrderDefinition.objects.create(name="OrderDefinition3")
-    order_definition3.charateristic_values.add(length, width, height)
-    order_definitions = [order_definition1, order_definition2, order_definition3]
-    for i in range(10):
-        item = MeasurementItem.objects.create(sn=i, name='Item {:d}'.format(i))
-        order = MeasurementOrder.objects.create(date=FAKE_TIME + datetime.timedelta(days=i),
-                                                order_type=order_definitions[i % 3])
-        order.measurement_items.add(item)
 
 
 def test_login_requierd(admin_client, live_server):
