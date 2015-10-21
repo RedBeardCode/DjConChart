7f01abf98c1aaf5ed708ab4b920848ddf3825fb0
farmer <farmi@web.de>
2015-10-21 14:16:21 +0300

    Rename MeasurementView in NewMeasurement

--- MeasurementManagement/urls.py
+++ MeasurementManagement/urls.py
@@ -4,12 +4,12 @@ from django.conf.urls import patterns, url
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 
-from .views import MeasurementView, get_ajax_order_info
+from .views import NewMeasurement, get_ajax_order_info
 
 admin.autodiscover()
 
 
 urlpatterns = patterns('MeasurementManagement.views',
-                       url(r'^new_measurement/$', login_required(MeasurementView.as_view()), name="new_measurement"),
+                       url(r'^new_measurement/$', login_required(NewMeasurement.as_view()), name="new_measurement"),
                        url(r'^get_order_info/$', get_ajax_order_info, name='get_order_info'),
                       )
--- MeasurementManagement/views.py
+++ MeasurementManagement/views.py
@@ -8,14 +8,14 @@ from django.http import JsonResponse
 from .models import Measurement, MeasurementOrder
 
 
-class MeasurementView(CreateView):
+class NewMeasurement(CreateView):
     template_name = "new_measurement.html"
     model = Measurement
     fields = ['date', 'order', 'order_items', 'examiner', 'remarks',
               'meas_item', 'measurement_devices', 'raw_data_file']
 
     def get_form(self, form_class=None):
-        form = super(MeasurementView, self).get_form(form_class)
+        form = super(NewMeasurement, self).get_form(form_class)
         field = form.fields['date']
         field.initial = datetime.now()
         field.widget = AdminSplitDateTime()
