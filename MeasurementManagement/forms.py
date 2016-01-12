41d9cf6156d4276901f5d787d72d2a17e32ad982
farmer <farmi@web.de>
2016-01-12 21:12:55 +0200

    28 Error message for requierd Fields with many Meas Items  work, tests misssing

--- MeasurementManagement/forms.py
+++ MeasurementManagement/forms.py
@@ -1,3 +1,6 @@
+from copy import copy
+
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 
 from .models import MeasurementItem, MeasurementOrder
@@ -8,6 +11,17 @@ class NewMeasurementItemForm(ModelForm):
         model = MeasurementItem
         fields = ['sn', 'name']
 
+    def clean(self):
+        clean_data = super(NewMeasurementItemForm, self).clean()
+        if '' in self.data['sn'] and not self.errors:
+            self.errors['sn'] = ['This field is required']
+        sn_list = copy(self.data.getlist('sn'))
+        sn_list.sort()
+        while sn_list and '' in sn_list:
+            sn_list.remove('')
+        if len(set(sn_list)) != len(sn_list):
+            raise ValidationError('Duplicated measurement item')
+        return clean_data
 
 class NewMeasurementOrderForm(ModelForm):
     class Meta:
