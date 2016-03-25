a90a1fd185d523c8beafad68750aefcd9dd3f5f6
farmer <farmi@web.de>
2016-03-25 18:43:48 +0100

    MM-2 Save Plot parameters in db (Teamcity test) #MM-2 Fixed

--- MeasurementManagement/models.py
+++ MeasurementManagement/models.py
@@ -337,7 +337,7 @@ post_save.connect(after_characteristic_value_saved, sender=CharacteristicValue)
 
 
 class PlotQueryFilter(models.Model):
-    description = models.CharField(max_length=128, verbose_name='Description of the plotted data')
+    description = models.CharField(max_length=100, verbose_name='Description of the plotted data')
     short_name = models.URLField(verbose_name='Short name of configuration. Also used for url', )
     _filter_args = models.TextField(verbose_name='Pickle of list of dictionaries with filter lookup strings')
     _plot_args = models.TextField(verbose_name='Pickle of List of dictionaries with plot parameter')
