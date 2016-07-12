5f7c19c55715d790ae5194aa2a3311ca83ba185f
RedBeardCode <s.farmbauer@red-beard-code.de>
2016-07-12 06:20:49 +0200

    Add travis support

--- djcon_chart/test_settings.py
+++ djcon_chart/test_settings.py
@@ -5,3 +5,15 @@ Overwrites settings for the unit tests
 from .settings import *  # pylint: disable=W0401, W0614
 
 MEASUREMENT_FILE_DIR = os.path.join(BASE_DIR, 'test_data')
+
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'travisci',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '',
+
+        }
+    }
