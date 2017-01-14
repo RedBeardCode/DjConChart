ebc0a7bfa57b3aa2bffaaca52da61886e5e93276
Stefan Farmbauer <s.farmbauer@red-beard-code.de>
2017-01-14 12:26:03 +0100

    Download file form bokeh and serve it to the browser
    To keep the setup simple

--- djcon_chart/views.py
+++ djcon_chart/views.py
@@ -6,6 +6,7 @@ Views for the djcon_chart project
 """
 import django.contrib.auth.views as auth_views
 from django.shortcuts import redirect
+from django.http import HttpResponse
 
 
 def login(request, *args, **kwargs):
@@ -25,9 +26,13 @@ def bokeh_redirect(request, *args, **kwargs):
     """
     Redirect to bokeh server to get the bokeh session
     """
+    from urllib.request import urlopen
     url = 'http://localhost:5006/autoload.js?bokeh-autoload-element={0}' \
           '&bokeh-session-id={1}'.format(
         request.GET['bokeh-autoload-element'],
         request.GET['bokeh-session-id']
     )
-    return redirect(url)
+    response = urlopen(url)
+    html = response.read()
+
+    return HttpResponse(html)
