aac1591ce6747d50edc069e2053faf488f2a6114
Stefan Farmbauer <s.farmbauer@red-beard-code.de>
2017-01-14 18:17:04 +0100

    Remove heroku static dir

--- djcon_chart/settings.py
+++ djcon_chart/settings.py
@@ -113,7 +113,6 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(BASE_DIR, 'static'),
-    r'.heroku/python/lib/python3.5/site-packages/bokeh/server/static',
 )
 try:
     import whitenoise
