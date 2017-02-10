c3ea1c7e0e0f56eb931be1aa391970a9ce049cb6
Stefan Farmbauer <s.farmbauer@red-beard-code.de>
2017-02-10 15:48:39 +0100

    Changed travis database settings to postgis

--- djcon_chart/test_settings.py
+++ djcon_chart/test_settings.py
@@ -9,7 +9,7 @@ MEASUREMENT_FILE_DIR = os.path.join(BASE_DIR, 'test_data')
 if 'TRAVIS' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'ENGINE': 'django.contrib.gis.db.backends.postgis',
             'NAME': 'travisci',
             'PASSWORD': '',
             'HOST': 'localhost',
